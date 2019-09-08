723fd4240482d0833797ff4b827a8252e55e80c3
Jordyn <onlinecloud1@gmail.com>
2019-09-08 11:17:08 -0500

    Add a new error relating to verification.

---
+++ errors.py
@@ -0,0 +1,27 @@
+import json
+
+class APIError(Exception):
+    httpcode = 400
+
+    @property
+    def json(self):
+        raise NotImplementedError("subclasses gotta implement this!")
+
+class NoUserFoundError(APIError):
+    @property
+    def json(self):
+        return json.dumps({"error": "ERRNOUSERFOUND"})
+
+class InvalidVerifyToken(APIError):
+    @property
+    def json(self):
+        return json.dumps({"error": "ERRINVALIDVERIFYTOKEN"})
+
+# This really isn't an error per-se.
+# We just use it to return an error on methods that don't return anything else.
+class NoError(APIError):
+    httpcode = 200
+
+    @property
+    def json(self):
+        return json.dumps({"error": "None", "result": "ok"})
